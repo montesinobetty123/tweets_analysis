64f10058d017089384c4752f35e7b0ce332cc483
betty <bettymontesino123@gmail.com>
2018-07-31 14:33:42 -0400

    add sentiment analysis

--- twitter_analysis.py
+++ twitter_analysis.py
@@ -3,12 +3,22 @@ from textblob import TextBlob
 import matplotlib.pyplot as plt
 
 #Get the JSON data
-tweetFile = open("TwitterData/tweets_small.json", "r")
+tweetFile = open("./tweets.json", "r")
 tweetData = json.load(tweetFile)
 tweetFile.close()
 
 # Continue your program below!
+## json object or tweets are store in tweetData
 
+#iterate list of tweet_small
+    #for each tweet access the text. use the text field tweet. tweet.text
+    #print polarity
+
+for tweet in tweetData:
+    #print(tweet["text"])
+
+    b= TextBlob(tweet["text"])
+    print(b.polarity)
 # Textblob sample:
 tb = TextBlob("You are a brilliant computer scientist.")
 print(tb.polarity)
